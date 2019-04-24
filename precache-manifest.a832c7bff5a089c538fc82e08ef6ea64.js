28072b11438cfaab9dad0edafbec34ffd5838308
jtCodes <jt234wq@gmail.com>
2019-04-24 16:16:08 -0400

    Updates

---
+++ precache-manifest.a832c7bff5a089c538fc82e08ef6ea64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880d83481580775721622aa429185e74",
+    "url": "/twist-pie-chart/index.html"
+  },
+  {
+    "revision": "b04900939dc03184079e",
+    "url": "/twist-pie-chart/static/css/main.2586cf39.chunk.css"
+  },
+  {
+    "revision": "8d05eeb67861a74644c7",
+    "url": "/twist-pie-chart/static/js/2.00cc60f5.chunk.js"
+  },
+  {
+    "revision": "b04900939dc03184079e",
+    "url": "/twist-pie-chart/static/js/main.5dc241ac.chunk.js"
+  },
+  {
+    "revision": "2f6c88c1fe997a6cc134",
+    "url": "/twist-pie-chart/static/js/runtime~main.a81528ab.js"
+  }
+]);
